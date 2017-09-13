b825babeac3221d4f355a79920a658a369ddde12
Jhonson2 <1750896420@qq.com>
2017-09-13 18:10:32 +0800

    BottomBar的集成+Badges徽章的使用

--- app/src/main/java/com/example/dellc/qq/MainActivity.java
+++ app/src/main/java/com/example/dellc/qq/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.dellc.qq;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.widget.FrameLayout;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.BottomBarTab;
+import com.roughike.bottombar.OnTabReselectListener;
+import com.roughike.bottombar.OnTabSelectListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseActivity {
+
+
+    @BindView(R.id.fragment_container)
+    FrameLayout mFragmentContainer;
+    @BindView(R.id.bottomBar)
+    BottomBar mBottomBar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int getLayoutResID() {
+        return R.layout.activity_main;
     }
+
+    @Override
+    protected void init() {
+        super.init();
+        mBottomBar.setOnTabSelectListener(mOnTabSelectListener);
+        BottomBarTab tabWithId=mBottomBar.getTabWithId(R.id.conversation);
+        tabWithId.setBadgeCount(10);
+    }
+
+    //tab的事件监听
+    private OnTabSelectListener mOnTabSelectListener=new OnTabSelectListener() {
+        @Override
+        public void onTabSelected(@IdRes int tabId) {
+
+        }
+    };
+
+
 }

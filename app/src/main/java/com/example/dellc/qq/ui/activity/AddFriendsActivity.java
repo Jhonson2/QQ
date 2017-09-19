12299093b12795a154cce274ab2f1c7d0e39a398
Jhonson2 <1750896420@qq.com>
2017-09-19 15:46:08 +0800

    添加好友界面的初始化实现

--- app/src/main/java/com/example/dellc/qq/ui/activity/AddFriendsActivity.java
+++ app/src/main/java/com/example/dellc/qq/ui/activity/AddFriendsActivity.java
@@ -1,14 +1,32 @@
 package com.example.dellc.qq.ui.activity;
 
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
 import com.example.dellc.qq.R;
-import com.example.dellc.qq.ui.fragment.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by dellc on 2017/9/19.
  */
 public class AddFriendsActivity extends BaseActivity {
+    @BindView(R.id.title)
+    TextView mTitle;
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+
     @Override
     public int getLayoutResID() {
         return R.layout.activity_add_friends;
     }
+
+    @Override
+    protected void init() {
+        super.init();
+        mTitle.setText(getString(R.string.add_friends));
+    }
+
 }
